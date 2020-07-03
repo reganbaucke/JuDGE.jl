64d1a0b823045e79008155684878660f48c30098
adow031 <a.downward@auckland.ac.nz>
2020-07-04 11:46:14 +1200

    Reworked deterministic equivalent code. Now much faster.

--- src/deteq.jl
+++ src/deteq.jl
@@ -3,7 +3,6 @@
 # a single investment along any path in the tree.
 struct DetEqModel
    problem::JuMP.Model
-
    function DetEqModel(tree, probability_function, sub_problem_builder, solver; discount_factor=1.0)
       println("Establishing deterministic equivalent model for tree: " * string(tree))
       probabilities = probability_function(tree)
@@ -19,7 +18,6 @@ struct DetEqModel
    end
 end
 
-
 function build_deteq(sub_problems, tree::T where T <: AbstractTree, probabilities, solver, discount_factor::Float64)
     model = JuMP.Model(solver)
 
@@ -30,27 +28,25 @@ function build_deteq(sub_problems, tree::T where T <: AbstractTree, probabilitie
 
     model.ext[:vars]=Dict()
     constant=0.0
-    for node in keys(sub_problems)
-        sp=sub_problems[node]
-
+    for (node,sp) in sub_problems
         objfn=objective_function(sp)
         constant+=objfn.constant
 
         model.ext[:vars][node] = Dict()
         for variable in all_variables(sp)
-            model.ext[:vars][node][string(variable)] = (variable,JuDGE.copy_variable!(model, variable))
+            model.ext[:vars][node][variable] = JuDGE.copy_variable!(model, variable)
             if variable in keys(objfn.terms)
-                set_objective_coefficient(model,model.ext[:vars][node][string(variable)][2],objfn.terms[variable])
+                set_objective_coefficient(model,model.ext[:vars][node][variable],objfn.terms[variable])
             end
         end
 
         for con in all_constraints(sp)
             LHS=AffExpr(0.0)
-            for (vv,v) in model.ext[:vars][node]
-                try
-                    add_to_expression!(LHS,normalized_coefficient(con,v[1]),v[2])
-                catch
-                    add_to_expression!(LHS,1,v[2])
+            if typeof(JuMP.constraint_object(con).func)==VariableRef
+                add_to_expression!(LHS,1,model.ext[:vars][node][JuMP.constraint_object(con).func])
+            else
+                for (v,c) in JuMP.constraint_object(con).func.terms
+                    add_to_expression!(LHS,c,model.ext[:vars][node][v])
                 end
             end
             set=JuMP.constraint_object(con).set
@@ -66,47 +62,35 @@ function build_deteq(sub_problems, tree::T where T <: AbstractTree, probabilitie
         end
     end
 
-    for node in keys(sub_problems)
+    for (node,sp) in sub_problems
         df=discount_factor^depth(node)
-        sp=sub_problems[node]
-        for variable in all_variables(sp)
-            for (name,exps) in sp.ext[:expansions]
-                if isa(exps,VariableRef)
-                    if string(exps)==string(variable)
-                        model.ext[:vars][node][string(variable)*"_master"] =JuDGE.copy_variable!(model, variable)
-                        set_objective_coefficient(model,model.ext[:vars][node][string(variable)*"_master"],df*probabilities(node)*JuDGE.coef(sp.ext[:expansioncosts],variable))
-                    end
-                elseif typeof(exps) <: AbstractArray
-                    for expvar in exps
-                        if string(expvar)==string(variable)
-                            model.ext[:vars][node][string(variable)*"_master"] =JuDGE.copy_variable!(model, variable)
-                            set_objective_coefficient(model,model.ext[:vars][node][string(variable)*"_master"],df*probabilities(node)*JuDGE.coef(sp.ext[:expansioncosts],variable))
-                        end
-                    end
+        for (name,exps) in sp.ext[:expansions]
+            if isa(exps,VariableRef)
+                variable=sp[name]
+                model.ext[:vars][node][string(variable)*"_master"] =JuDGE.copy_variable!(model, variable)
+                set_objective_coefficient(model,model.ext[:vars][node][string(variable)*"_master"],df*probabilities(node)*JuDGE.coef(sp.ext[:expansioncosts],variable))
+            elseif typeof(exps) <: AbstractArray
+                variables=sp[name]
+                for index in eachindex(exps)
+                    model.ext[:vars][node][string(variables[index])*"_master"] =JuDGE.copy_variable!(model, variables[index])
+                    set_objective_coefficient(model,model.ext[:vars][node][string(variables[index])*"_master"],df*probabilities(node)*JuDGE.coef(sp.ext[:expansioncosts],variables[index]))
                 end
             end
         end
     end
 
-    for node in keys(sub_problems)
-        sp=sub_problems[node]
-        for variable in all_variables(sp)
-            for (name,exps) in sp.ext[:expansions]
-                if isa(exps,VariableRef)
-                    if string(exps)==string(variable)
-                        @constraint(model,model.ext[:vars][node][string(variable)][2]<=sum(model.ext[:vars][n][string(variable)*"_master"] for n in history(node)))
-                        if typeof(node)==Leaf
-                            @constraint(model,sum(model.ext[:vars][n][string(variable)*"_master"] for n in history(node))<=1)
-                        end
-                    end
-                elseif typeof(exps) <: AbstractArray
-                    for expvar in exps
-                        if string(expvar)==string(variable)
-                            @constraint(model,model.ext[:vars][node][string(variable)][2]<=sum(model.ext[:vars][n][string(variable)*"_master"] for n in history(node)))
-                            if typeof(node)==Leaf
-                                @constraint(model,sum(model.ext[:vars][n][string(variable)*"_master"] for n in history(node))<=1)
-                            end
-                        end
+    for (node,sp) in sub_problems
+        for (name,exps) in sp.ext[:expansions]
+            if isa(exps,VariableRef)
+                @constraint(model,model.ext[:vars][node][exps]<=sum(model.ext[:vars][n][string(exps)*"_master"] for n in history(node)))
+                if typeof(node)==Leaf
+                    @constraint(model,sum(model.ext[:vars][n][string(exps)*"_master"] for n in history(node))<=1)
+                end
+            elseif typeof(exps) <: AbstractArray
+                for expvar in exps
+                    @constraint(model,model.ext[:vars][node][expvar]<=sum(model.ext[:vars][n][string(expvar)*"_master"] for n in history(node)))
+                    if typeof(node)==Leaf
+                        @constraint(model,sum(model.ext[:vars][n][string(expvar)*"_master"] for n in history(node))<=1)
                     end
                 end
             end
