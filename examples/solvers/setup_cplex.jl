ef0fc7a45003606deba492401d306003e1656e7e
odow <o.dowson@gmail.com>
2021-06-09 20:02:32 +1200

    Add a basic test harness and CI

--- examples/solvers/setup_cplex.jl
+++ examples/solvers/setup_cplex.jl
@@ -1,17 +1,27 @@
-using CPLEX
+import CPLEX
+using JuMP
 
-JuDGE_MP_Solver = optimizer_with_attributes(CPLEX.Optimizer, "CPXPARAM_SolutionType" => 2,
-                                                             "CPXPARAM_MIP_Tolerances_AbsMIPGap" => 0.0,
-                                                             "CPXPARAM_MIP_Tolerances_Integrality" => 10^-6,
-															 "CPX_PARAM_SCRIND" => 0,
-                                                             "CPXPARAM_MIP_Display" => 0,
-                                                             "CPXPARAM_ParamDisplay" => 0)
+JuDGE_MP_Solver = optimizer_with_attributes(
+    CPLEX.Optimizer,
+    "CPXPARAM_SolutionType" => 2,
+    "CPXPARAM_MIP_Tolerances_AbsMIPGap" => 0.0,
+    "CPXPARAM_MIP_Tolerances_Integrality" => 10^-6,
+    "CPX_PARAM_SCRIND" => 0,
+    "CPXPARAM_MIP_Display" => 0,
+    "CPXPARAM_ParamDisplay" => 0,
+)
 
-JuDGE_SP_Solver = optimizer_with_attributes(CPLEX.Optimizer, "CPXPARAM_MIP_Tolerances_MIPGap" => 0.0,
-                                                             "CPXPARAM_MIP_Tolerances_Integrality" => 10^-6,
-															 "CPX_PARAM_SCRIND" => 0,
-                                                             "CPXPARAM_ParamDisplay" => 0)
+JuDGE_SP_Solver = optimizer_with_attributes(
+    CPLEX.Optimizer,
+    "CPXPARAM_MIP_Tolerances_MIPGap" => 0.0,
+    "CPXPARAM_MIP_Tolerances_Integrality" => 10^-6,
+    "CPX_PARAM_SCRIND" => 0,
+    "CPXPARAM_ParamDisplay" => 0,
+)
 
-JuDGE_DE_Solver = optimizer_with_attributes(CPLEX.Optimizer, "CPXPARAM_MIP_Tolerances_MIPGap" => 0.0,
-                                                          	 "CPXPARAM_MIP_Tolerances_Integrality" => 10^-6,
-															 "CPX_PARAM_SCRIND" => 1)
+JuDGE_DE_Solver = optimizer_with_attributes(
+    CPLEX.Optimizer,
+    "CPXPARAM_MIP_Tolerances_MIPGap" => 0.0,
+    "CPXPARAM_MIP_Tolerances_Integrality" => 10^-6,
+    "CPX_PARAM_SCRIND" => 1,
+)
