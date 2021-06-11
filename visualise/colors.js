62cc53d8f7f74006df10c6733c8c1791dba915f9
Tony Downward <adow031@UoA.auckland.ac.nz>
2021-06-11 13:54:15 +1200

    Updated visualisation html template.

--- visualise/colors.js
+++ visualise/colors.js
@@ -1,4 +1,6 @@
-var colorTemplates = {greyyellowred:[{weight:0.0,color:[0.6,0.6,0.6]},{weight:0.2,color:[0.8,0.8,0.0]},{weight:1.0,color:[1.0,0.0,0.0]}],
+var colorTemplates = {blackredgreenblue:[{weight:0.0,color:[0.0,0.0,0.0]},{weight:0.333,color:[0.85,0.0,0.0]},{weight:0.667,color:[0.0,0.8,0.0]},{weight:1.0,color:[0.0,0.0,0.85]}],
+blackgreenblue:[{weight:0.0,color:[0.0,0.0,0.0]},{weight:0.5,color:[0.0,0.8,0.0]},{weight:1.0,color:[0.0,0.0,0.85]}],
+greyyellowred:[{weight:0.0,color:[0.6,0.6,0.6]},{weight:0.2,color:[0.8,0.8,0.0]},{weight:1.0,color:[1.0,0.0,0.0]}],
 blackwhite:[{weight:0.0,color:[0.0,0.0,0.0]},{weight:1.0,color:[1.0,1.0,1.0]}],
 whiteblack:[{weight:0.0,color:[1.0,1.0,1.0]},{weight:1.0,color:[0.0,0.0,0.0]}],
 greenblue:[{weight:0.0,color:[0.0,0.7,0.5]},{weight:1.0,color:[0.0,0.0,0.9]}],
